5100593bbf41547d50c2ba320caa03f4a758c9e5
vridhi <rajeevvridhi@gmail.com>
2026-09-08 18:52:43 +0530

    feat: implement MongoDB connection and integrate with server startup

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express'; 
+import { connectDB } from './db/connectDB.js';
 
 const app = express(); 
 
@@ -7,5 +8,6 @@ app.get('/' , (req,res)=>{
 })
 
 app.listen(3000 , ()=>{
+    connectDB(); 
     console.log("server is running on port 3000"); 
 })
