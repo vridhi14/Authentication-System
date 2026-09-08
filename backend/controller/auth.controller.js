0ce64cbb19e5cc8364526081c0e8f427270cfc58
vridhi <rajeevvridhi@gmail.com>
2026-09-08 19:16:49 +0530

    feat: implement authentication controller and update routes to use controller methods

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = async(req,res)=>{
+    res.send("signup route"); 
+}
+export const logout= async(req,res)=>{
+    res.send("logout route"); 
+}
+export const login = async(req,res)=>{
+    res.send("login  route"); 
+}
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,15 +1,10 @@
 import express from 'express' ; 
+import { login, logout, signup } from '../controller/auth.controller';
 const router  = express.Router(); 
 
-router.get("/signup", (req,res)=>{
-    res.send("signup route"); 
-})
-router.get("/login", (req,res)=>{
-    res.send("login route"); 
-})
-router.get("/logout", (req,res)=>{
-    res.send("logout route"); 
-})
+router.get("/signup", signup); 
+router.get("/login", login );
+router.get("/logout", logout);
 
 
 export default router ; 
